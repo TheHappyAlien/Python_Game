0b8efdc15538b98f3d5f0c7b124dddfb92a03633
Aleksander Musz <olekmusz@gmail.com>
2023-06-20 22:30:08 +0200

    work Please

--- Scripts/move_speed_upgrade.py
+++ Scripts/move_speed_upgrade.py
@@ -4,7 +4,7 @@ from upgrade_shop import Upgrade_shop
 
 class Move_speed_upgrade(Upgrade_shop):
     def __init__(self, pos, player, display_surface) -> None:
-        super().__init__(pos, image=pygame.image.load("../Sprites/move_speed_boost.png"), player=player, cost=20, display_surface=display_surface, rect_left_offset=-10, rect_top_offset=-20, rect_width=32, rect_height=32, text_x_offset=-8, text_y_offset=40)
+        super().__init__(pos, image=pygame.image.load("./Sprites/move_speed_boost.png"), player=player, cost=20, display_surface=display_surface, rect_left_offset=-10, rect_top_offset=-20, rect_width=32, rect_height=32, text_x_offset=-8, text_y_offset=40)
 
     def update(self, x_shift, y_shift) -> None:
         super().update(x_shift, y_shift)
