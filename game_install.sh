37d8cd661d9a8791215eb2ad4755c9ada48b065b
TheHappyAlien <126973515+TheHappyAlien@users.noreply.github.com>
2023-06-21 14:23:06 +0200

    Add files via upload

---
+++ game_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git clone https://github.com/TheHappyAlien/Python_Game.git
+cd ./Python_Game
+
+python -m venv .venv
+source .venv/bin/activate
+
+pip install -r requirements.txt
+
+pyinstaller ./Scripts/main.py --onefile --noconsole
+
+mv ./SoundEffects ./dist
+mv ./Sprites ./dist
+mv ./high_score.txt ./dist/high_score.txt
+rm -r ./Scripts
+
+cd ./dist
+
+mv ./main ./PythonShooter
+./PythonShooter
